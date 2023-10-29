176e6fed9732771841820995b9b00be30e496f90
seimonik <simonik1002@mail.ru>
2023-10-30 00:11:49 +0400

    fix checkstyle

--- src/main/java/edu/hw3/Task4.java
+++ src/main/java/edu/hw3/Task4.java
@@ -1,35 +1,43 @@
 package edu.hw3;
 
 public class Task4 {
+    static final int M = 1_000;
+    static final int D = 500;
+    static final int C = 100;
+    static final int L = 50;
+    static final int X = 10;
+    static final int V = 5;
+    static final int I = 1;
+
     public static String convertToRoman(int number){
         StringBuilder result = new StringBuilder();
 
-        while (number >= 1000) {
-            number -= 1000;
+        while (number >= M) {
+            number -= M;
             result.append('M');
         }
-        while (number >= 500){
-            number -= 500;
+        while (number >= D){
+            number -= D;
             result.append('D');
         }
-        while (number >= 100){
-            number -= 100;
+        while (number >= C){
+            number -= C;
             result.append('C');
         }
-        while (number >= 50){
-            number -= 50;
+        while (number >= L){
+            number -= L;
             result.append('L');
         }
-        while (number >= 10){
-            number -= 10;
+        while (number >= X){
+            number -= X;
             result.append('X');
         }
-        while (number >= 5){
-            number -= 5;
+        while (number >= V){
+            number -= V;
             result.append('V');
         }
-        while (number >= 1){
-            number -= 1;
+        while (number >= I){
+            number -= I;
             result.append('I');
         }
 
